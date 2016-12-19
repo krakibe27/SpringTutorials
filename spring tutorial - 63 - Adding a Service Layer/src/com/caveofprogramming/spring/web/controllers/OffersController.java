a639c17c372b030b3ba8d322fa89ab7341cfe84e
KAPIL RAKIBE <krakibe27@gmail.com>
2016-12-19 10:18:44 -0800

    Add files via upload
    Adding a Service Layer

---
+++ spring tutorial - 63 - Adding a Service Layer/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,34 @@
+package com.caveofprogramming.spring.web.controllers;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
+
+
+@Controller
+public class OffersController {
+	
+	@Autowired
+	private OffersService offersService;
+
+	@RequestMapping("/")	
+	public String showHome(Model model){
+		
+	List<Offer> offer = offersService.getCurrent();
+	
+		model.addAttribute("offers", offer);
+		
+		return "home";
+	}
+
+}
+
